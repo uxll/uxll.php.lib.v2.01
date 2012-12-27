3b440d75b943c32794383cb59f0473d7128a1b49
uxll <uxll@qq.com>
2012-12-27 09:09:28 +0800

    smarty added

---
+++ privateData/dbConnect.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  @version: 2.01
+ *  @date: 2012-12-27 
+ *  @author: uxll@qq.com
+ *  @file: dbConnect.php
+ */
+return array(
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'wap-5ameili-cn',
+	'charset' => 'utf8'
+);
